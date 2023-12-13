e7f22d39c3d30452e709563cfe8236186a13f8e5
dirtgrubdylan <dman1366@gmail.com>
2023-12-13 17:01:16 +0000

    Make templates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod util;
 
 mod day_1;
+mod day_10;
 mod day_2;
 mod day_3;
 mod day_4;
@@ -9,7 +10,6 @@ mod day_6;
 mod day_7;
 mod day_8;
 mod day_9;
-mod day_10;
 
 use std::io::{self, Write};
 
